3165be32f96f583bdd9008e88ffc7dacf3825234
kenshin11514 <kenshin_fetalvero@dlsu.edu.ph>
2023-07-24 14:42:55 +0800

    fixed voting bug

--- public/js/voting.js
+++ public/js/voting.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $('.upvote').on('click', function () {
         var review_id = $(this).attr('data');
+        var upvoteLabel = $(this).prevAll('.numUpvotes').first();
+        var downvoteLabel = $(this).nextAll('.numDownvotes').first();
 
         $.get('/upvote', { review_id: review_id }, function (data) {
 
@@ -8,23 +10,26 @@ $(document).ready(function () {
                 console.log('success');
 
                 // change the number of upvotes and downvotes using jquery
-                $(this).siblings('.numUpvotes').text(data.numUpvotes);
-                $(this).siblings('.numDownvotes').text(data.numDownvotes);
+                $(upvoteLabel).text(data.numUpvotes);
+                $(downvoteLabel).text(data.numDownvotes);
             }
         });
     });
 
     $('.downvote').on('click', function () {
         var review_id = $(this).attr('data');
+        var upvoteLabel = $(this).prevAll('.numUpvotes').first();
+        var downvoteLabel = $(this).prevAll('.numDownvotes').first();
+
 
         $.get('/downvote', { review_id: review_id }, function (data) {
 
+            console.log(data);
             if (data.success) {
                 console.log('success');
-
                 // change the number of upvotes and downvotes using jquery
-                $(this).siblings('.numUpvotes').text(data.numUpvotes);
-                $(this).siblings('.numDownvotes').text(data.numDownvotes);
+                $(upvoteLabel).text(data.numUpvotes);
+                $(downvoteLabel).text(data.numDownvotes);          
             }
 
         });
